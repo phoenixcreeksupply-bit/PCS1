3107dc60e3c826a3614651a1d2d6ef8c2c19d2a7
phoenixcreeksupply-bit <phoenixcreeksupply@gmail.com>
2025-08-18 08:56:19 -0600

    Update affiliatelinks.js

--- data/affiliatelinks.js
+++ data/affiliatelinks.js
@@ -1,1 +1,9 @@
+const affiliateLinks = {
+  amazon: "https://www.amazon.com/?tag=YOURTAGHERE",
+  husqvarna: "https://www.acmetools.com/husqvarna-axe/",
+  garrett: "https://www.kellycodetectors.com/garrett-ace-400/",
+  kuhl: "https://www.kuhl.com/kuhl/mens/pants/renegade-rock/",
+  gumroad: "https://monroe.gumroad.com/l/modernstoic"
+};
 
+export default affiliateLinks;
